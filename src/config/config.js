1a546829e5175d255d7ad1515c5ed6380c68a7c4
Wylakers <rodrigop_23@outlook.es>
2022-11-30 16:56:28 -0500

    json removed

--- src/config/config.js
+++ src/config/config.js
@@ -7,8 +7,8 @@
     dialect: process.env.DIALECT || "postgres";
     port: process.env.DBPORT || 5433;
     seederStorage: "json";
-    seederStoragePath: "sequelizeSeeds.json"
-  },
+    seederStoragePath: "sequelizeSeeds.json";
+  }
   test: {
     username: process.env.DBUSER || "postgres";
     password: process.env.PASSWORD || "123";
@@ -17,8 +17,8 @@
     dialect: process.env.DIALECT || "postgres";
     port: process.env.DBPORT || 5433;
     seederStorage: "json";
-    seederStoragePath: "sequelizeSeeds.json"
-  },
+    seederStoragePath: "sequelizeSeeds.json";
+  }
   production: {
     username: process.env.DBUSER || "postgres";
     password: process.env.PASSWORD || "123";
@@ -27,6 +27,6 @@
     dialect: process.env.DIALECT || "postgres";
     port: process.env.DBPORT || 5433;
     seederStorage: "json";
-    seederStoragePath: sequelizeSeeds.json
+    seederStoragePath: sequelizeSeeds.json;
   }
 }
