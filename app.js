4b32fa4cc1e8da6660c6adff1558bba1adfd6683
Wylakers <rodrigop_23@outlook.es>
2022-12-03 08:53:22 -0500

    final backend

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 
-// import usuarioRoutes from "./src/routes/Usuario.routes.js";
+import usuarioRoutes from "./src/routes/Usuario.routes.js";
 import detalleRoutes from "./src/routes/Detalle.routes.js";
 import categoriaRoutes from "./src/routes/Categoria.routes.js";
 import productoRoutes from "./src/routes/Producto.routes.js";
 import preArmadoRoutes from "./src/routes/PreArmado.routes.js";
 import preArmadoProductoRoutes from "./src/routes/PreArmado-Producto.routes.js";
 import reporteRoutes from "./src/routes/Reporte.routes.js";
+import ordenRoutes from "./src/routes/Orden.routes.js";
+import ordenProductoRoutes from "./src/routes/Orden-Producto.routes.js";
+import reviewRoutes from "./src/routes/Review.routes.js";
+import tipoReviewRoutes from "./src/routes/TipoReview.routes.js";
 
 var app = express();
 
@@ -16,12 +20,16 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
-// app.use(usuarioRoutes);
+app.use(usuarioRoutes);
 app.use(detalleRoutes);
 app.use(categoriaRoutes);
 app.use(productoRoutes);
 app.use(preArmadoRoutes);
 app.use(preArmadoProductoRoutes);
 app.use(reporteRoutes);
+app.use(ordenRoutes);
+app.use(ordenProductoRoutes);
+app.use(reviewRoutes);
+app.use(tipoReviewRoutes);
 
 export default app;
